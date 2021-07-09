b5ed64ff90288b590727ebae4401889d06ea4133
yofio <65844842+yofio@users.noreply.github.com>
2021-07-09 15:51:43 +0900

    型定義を効率的に管理する

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,6 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completedMark = completed ? "[完]" : "[未]";
   return <p>{`${completedMark}${title}(ユーザー:${userId})`}</p>;
